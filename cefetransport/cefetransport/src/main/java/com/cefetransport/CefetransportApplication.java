38b56d5293ba8af875de63f3c8274b54dffd6c4e
Marcio Gabriel <gabriel09msn@gmail.com>
2023-05-01 18:00:35 -0300

    Projeto inicializado

---
+++ cefetransport/cefetransport/src/main/java/com/cefetransport/CefetransportApplication.java
@@ -0,0 +1,13 @@
+package com.cefetransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CefetransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CefetransportApplication.class, args);
+	}
+
+}
