f4f215f58bba00613d03ccb76ae837227959110a
Marcio Gabriel <gabriel09msn@gmail.com>
2023-05-08 00:13:29 -0300

    .

--- CefeTransport/src/main/java/com/cefetransport/CefeTransportApplication.java
+++ CefeTransport/src/main/java/com/cefetransport/CefeTransportApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CefetransportApplication {
+public class CefeTransportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CefetransportApplication.class, args);
+		SpringApplication.run(CefeTransportApplication.class, args);
 	}
 
 }
